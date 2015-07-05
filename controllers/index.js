36119f810f3b4b3c7446a3de57555316031e453a
Tombown <tombown@hotmail.co.uk>
2015-07-05 14:17:20 +0100

    Initial commit
    Original MVC framework

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var index = require('../models/index');
+var router = express.Router();
+var app = express();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+app.post('/login',
+  passport.authenticate('local'),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.redirect('/users/' + req.user.username);
+  });
+
+module.exports = router;
